09f8fa26d3faca1a1a09b8e4622f0e233458d188
Bhavana Jain <cs14btech11008@iith.ac.in>
2020-04-23 02:54:35 -0500

    [WIP] Defines Blockchain, adds new() and insert()

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -11,190 +11,148 @@ use crate::crypto::address::H160;
 extern crate chrono;
 use chrono::prelude::*;
 
+
+pub struct Metablock {
+    pub block: Block,
+    pub height: u32,
+}
+
 pub struct Blockchain {
-    pub chain:HashMap<H256,Block>,
-    pub tiphash:H256,
-    pub heights:HashMap<H256,u8>,
-    pub buffer:HashMap<H256,Block>,
-    pub totaldelay:i64,
+    pub proposer_chain: HashMap<H256, Metablock>,
+    pub proposer_tip: H256,
+    pub proposer_depth: u32,
+
+    pub voter_chains: Vec<HashMap<H256, Metablock>>,
+    pub voter_tips: Vec<H256>,
+    pub voter_depths: Vec<u32>,
+    // list of unreferenced proposer blocks
+    pub unref_proposers: Vec<H256>,
+    // Hash of first proposer block seen corresponding to each level
+    pub level2proposer: HashMap<u32, H256>,
+    // Last voted level corresponding to each voter chain 
+    pub chain2level: HashMap<u32, u32>
+
+    // TODO: add orphan buffer for proposer and voter chains 
 }
 
 impl Blockchain {
-    /// Create a new blockchain, only containing the genesis block
-    pub fn new() -> Self {
-        let buffer: [u8; 32] = [0; 32];
-        let b:H256 = buffer.into();
-        let genesis:Block = block::generate_genesis_block(&b);
-        let genhash:H256 = genesis.hash();
-        let mut chainmap:HashMap<H256,Block> = HashMap::new();
-        let mut heightsmap:HashMap<H256,u8> = HashMap::new();
-        let buffermap:HashMap<H256,Block> = HashMap::new();
-        chainmap.insert(genhash,genesis);
-        heightsmap.insert(genhash,0);
-        let t:H256 = genhash;
-        let newchain:Blockchain = Blockchain{chain:chainmap,tiphash:t,heights:heightsmap,buffer:buffermap,totaldelay:0};
-        newchain
+    pub fn new(m: u32) -> Self {
+        // genesis for proposer and voter chains
+        let mut proposer_chain = HashMap::new();
+        let proposer = genesis_proposer();
+        let proposer_hash = proposer.hash();
+        let metablock = Metablock {
+            block: proposer,
+            height: 1,
+        }
+        proposer_chain.insert(proposer_hash, metablock);
+        let proposer_tip = proposer_hash;
+
+        let mut voter_chains = Vec::new();
+        let mut voter_tips = Vec::new();
+        let mut chain2level = HashMap::new();
+        let mut voter_depths = Vec::new();
+        for i in 1..m {
+            let mut tmp_chain = HashMap::new();
+            voter = genesis_voter(i);
+            voter_hash = voter.hash();
+            let metablock = Metablock {
+                block: voter,
+                height: 1,
+            }
+            tmp_chain.insert(voter_hash, metablock);
+            voter_chains.push(tmp_chain);
+            voter_tips.push(voter_hash);
+            chain2level.insert(i, 0);
+            voter_depths.push(1);
+        } 
+
+        let mut unref_proposers = Vec::new();
+        unref_proposers.push(proposer_hash);
+
+        let mut level2proposer = HashMap::new();
+        level2proposer.insert(1, proposer_hash);
+
+        Blockchain {
+            proposer_chain: proposer_chain,
+            proposer_tip: proposer_hash,
+            proposer_depth: 1,
+            voter_chains: voter_chains,
+            voter_tips: voter_tips,
+            voter_depths: voter_depths,
+            unref_proposers: unref_proposers,
+            level2proposer: level2proposer,
+            chain2level: chain2level,
+        }
     }
 
-    /// Insert a block into blockchain
-    pub fn insert(&mut self, block: &Block,mut mempool:&mut TransactionMempool,mut blockstate:&mut BlockState) {
-
-
-        let h:H256 = block.hash();
-        //let mut flag:bool = false;
-
-
-        match self.chain.get(&block.header.parenthash){
-            Some(pblock) => { //insertion into mainchain
-                let mut validity:bool = false;
-                if h < pblock.header.difficulty && !self.chain.contains_key(&h) {
-                let b_delay = Local::now().timestamp_millis() - block.header.timestamp;
-                self.totaldelay = self.totaldelay + b_delay;
-                if blockstate.block_state_map.contains_key(&block.header.parenthash){
-                validity = is_blck_valid(&block,&blockstate.block_state_map.get(&block.header.parenthash).unwrap());
-                }
-                else {
-                println!("State of parent block not found");
-                return;
-                }
-                //checks and updates
-                if !validity {
-                    println!("Block with hash {} does not satisfy state validity",h);
-                    return;
+    pub fn insert(&mut self, block: &Block) {
+        // TODO: If block has missing refs, add to orphan buffermap
+        
+        // Haven't added all checks -- parent present then unwrap with confidence etc
+        match block.content {
+            Content::Proposer(c) => {
+                // add selfhash to unreferenced, remove referenced proposers in content
+                let block_hash = block.hash();
+                self.unref_proposers.push(block_hash);
+                for ref_proposer in block.content.proposer_refs {
+                    // remove from self.unref_proposers
+                    // TODO check what happens if ref_proposer not in self.unref_proposer. shouldn't panic
+                    let index = self.unref_proposers.iter().position(|x| *x == ref_proposer).unwrap();
+                    self.unref_proposers.remove(index);
                 }
                 
-
-                println!("Adding block with hash {} mined by  node {} to chain",h,block.header.miner_id);
-                println!("Block delay is: {:?}",(Local::now().timestamp_millis() - block.header.timestamp));
-                println!("Average delay is {}",self.totaldelay/(self.chain.len() as i64));
-                println!("Total number of blocks in blockchain:{}\n",self.chain.len());
-                self.chain.insert(h,block.clone());
-                mempool_update(&block,&mut mempool);
-                update_block_state(&block,&mut blockstate);
-                
-                let len = self.heights[&block.header.parenthash]+1;
-                self.heights.insert(h,len);
-                if len>self.heights[&self.tiphash] {
-                    self.tiphash = h;
-                    println!("Current tipheight is {}",len);
-                    println!("All blocks in longest chain: {:?}",self.all_blocks_in_longest_chain());
-                    
-
-                    let mut temp_state_map = blockstate.block_state_map.get(&block.hash()).unwrap();
-                    let mut utxo_hmap:HashMap<H160,u32> = HashMap::new();
-
-                    for (utxo_input,utxo_output) in temp_state_map.state_map.iter() {
-                        if !utxo_hmap.contains_key(&utxo_output.receipient_addr){
-                            utxo_hmap.insert(utxo_output.receipient_addr,utxo_output.value);
-                        }else{
-                            *utxo_hmap.get_mut(&utxo_output.receipient_addr).unwrap() = *utxo_hmap.get_mut(&utxo_output.receipient_addr).unwrap()+utxo_output.value;
-                        }
-                    }
-                    for (key,value) in utxo_hmap.iter() {
-                        println!("balance in addr {:?} is {:?}",key,value);
-                    }
+                let parent_meta = self.proposer_chain.get(&block.header.parenthash).unwrap();
+                let metablock = Metablock {
+                    block: block,
+                    height: parent_meta.height + 1
                 }
 
-                //let mut bhash_copy:H256 = hash::generate_random_hash();
-                //if stale blocks parent has arrived, insert it into main chain
-                let mut bhash_vec = Vec::new();
-                let mut phash_q: VecDeque<H256>= VecDeque::new();
-                phash_q.push_back(h);
-                while !phash_q.is_empty() {
-                    match phash_q.pop_front() {
-                        Some(h) => for (bhash,blck) in self.buffer.iter(){
-                                if blck.header.parenthash == h {
-                                    //flag = true;
-                                    let bhash_copy:H256 = *bhash;
-                                    bhash_vec.push(bhash_copy);
-                                    //checks and updates
-                                    let mut validity:bool = false;
-                                    if blockstate.block_state_map.contains_key(&h){
-                                        validity = is_blck_valid(&block,&blockstate.block_state_map.get(&h).unwrap());
-                                    }
-                                    else {
-                                        println!("State of parent block not found");
-                                        continue;
-                                    }
-                                    //let validity:bool = is_blck_valid(&block,&blockstate.block_state_map.get(&block.header.parenthash).unwrap());
-                                        if !validity {
-                                        println!("Block with hash {} does not satisfy state validity",h);
-                                        return;
-                                        }
-                                    
-                                
-
-                                    self.chain.insert(bhash_copy,blck.clone());
-                                    mempool_update(&blck,&mut mempool);
-                                    update_block_state(&blck,&mut blockstate);
-                                    let b_delay = Local::now().timestamp_millis() - blck.header.timestamp;
-                                    self.totaldelay = self.totaldelay + b_delay;
-
-                                    println!("Adding block with hash {} to chain",blck.hash());
-                                    println!("Block delay is: {:?}",(Local::now().timestamp_millis() - blck.header.timestamp));
-                                    println!("Average delay is {}",self.totaldelay/(self.chain.len() as i64));
-                                    println!("Total number of blocks in blockchain:{}\n",self.chain.len());
-                                    let len = self.heights[&blck.header.parenthash]+1;
-                                    self.heights.insert(bhash_copy,len);
-                                    if len>self.heights[&self.tiphash] {
-                                        self.tiphash = bhash_copy;
-                                    }
-                                }
-                            },
-                        None => (),
-                    }
+                // if this is the first proposer block at its level, update level2proposer map
+                if (!self.level2proposer.contains_key(&metablock.height)) {
+                    self.level2proposer.insert(metablock.height, block_hash);
                 }
 
-
-                for bh in bhash_vec{
-                    self.buffer.remove(&bh);
+                // add to proposer chain and update proposer tip if depth has increased
+                self.proposer_chain.insert(block_hash, metablock);
+                if metablock.height > self.proposer_depth {
+                    self.proposer_depth = metablock.height;
+                    self.proposer_tip = block_hash,
                 }
-             }
-            }, // insert stale block into buffer
-            _ => {
-                  print!("Adding block with hash {} to buffer\n",h); 
-                  if !self.buffer.contains_key(&h){
-                  self.buffer.insert(h,block.clone()); 
-                  }
-                 },
+            }
+
+            Content::Voter(c) => {
+                let chain_num = block.content.chain_num;
+                // calculate max level voted
+                let mut max_vote_level: u32 = self.chain2level[chain_num];
+                for vote in block.content.votes {
+                    // unwrap confidence?
+                    let block_level = self.proposer_chain.get(&vote).unwrap().height;
+                    let max_vote_level = max(max_vote_level, block_level);
+                }
+                self.chain2level.insert(chain_num, max_vote_level);
+
+                // add to voter chain and update tip if required
+                let parent_meta = self.voter_chains[chain_num-1].get(&block.header.parenthash).unwrap();
+                let metablock = Metablock {
+                    block: block,
+                    height: parent_meta.height + 1
+                }
+                self.voter_chains[chain_num-1].insert(block_hash, metablock);
+                if metablock.height > self.voter_depths[chain_num-1] {
+                    self.voter_depths[chain_num-1] = metablock.height;
+                    self.voter_tips[chain_num-1] = block_hash,
+                }
+            }
         }
-
     }
 
-    /// Get the last block's hash of the longest chain
-    pub fn tip(&self) -> H256 {
-        self.tiphash
+    pub fn get_proposer_tip(&self) {
+        self.proposer_tip
     }
 
-    /// Get the last block's hash of the longest chain
-    //#[cfg(any(test, test_utilities))]
-    pub fn all_blocks_in_longest_chain(&self) -> Vec<H256> {
-
-        let mut phash:H256 = self.tiphash;
-        let mut result:Vec<H256>=vec![];
-        let mut buffer: [u8; 32] = [0; 32];
-        let b:H256 = buffer.into();
-        while(phash!=b){
-            result.push(phash);
-            phash = self.chain[&phash].header.parenthash;
-        }
-        let mut res = result.reverse();
-        result
+    pub fn get_voter_tip(&self, chain_num) {
+        self.voter_tips[chain_num]
     }
-}
 
-#[cfg(any(test, test_utilities))]
-mod tests {
-    use super::*;
-    use crate::block;
-    use crate::crypto::hash::Hashable;
-
-    #[test]
-    fn insert_one() {
-        let mut blockchain = Blockchain::new();
-        let genesis_hash = blockchain.tip();
-        let block = block::generate_random_block(&genesis_hash);
-        blockchain.insert(&block);
-        assert_eq!(blockchain.tip(), block.hash());
-    }
 }
