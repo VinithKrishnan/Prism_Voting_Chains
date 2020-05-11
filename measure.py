f616007795136cd73c5669a19ca6fcc5beb229c2
Bhavana Jain <cs14btech11008@iith.ac.in>
2020-05-11 14:08:56 -0500

    measure average, use lambda sleep time in api mod

--- measure.py
+++ measure.py
@@ -1,40 +1,50 @@
 import re
 import sys
+from collections import defaultdict
 
+class Node:
+    def __init__(self):
+        self.delay = 0
+        self.num_entries = 0
 
-print(sys.argv)
+    def add_entry(self, sample):
+        self.delay += sample
+        self.num_entries += 1
 
-logfile = sys.argv[1]
+    def print_entry(self):
+        print('Delay %d Num entries %d Average %0.2f' % (self.delay, self.num_entries, (self.delay/self.num_entries)))
 
-pattern = re.compile("^Received trans hash")
-recv_time_map = {}
+delay_map = defaultdict(Node)
 
-with open(logfile, 'r') as f:
-    for line in f:
-    # lines = f.readlines()
-    # for line in lines:
-        if pattern.match(line):
-            separated = line.split()
-            tx_hash = separated[3]
-            recv_time = int(separated[-1])
-            # extract the hash and time 
-            recv_time_map[tx_hash] = recv_time 
+for logfile in ['p1.out', 'p2.out', 'p3.out']:
+    pattern = re.compile("^Received trans hash")
+    recv_time_map = {}
+    with open(logfile, 'r') as f:
+        for line in f:
+            if pattern.match(line):
+                separated = line.split()
+                tx_hash = separated[3]
+                recv_time = int(separated[-1])
+                # extract the hash and time 
+                recv_time_map[tx_hash] = recv_time 
 
+    pattern = re.compile("^Confirmed trans hash")
+    confirm_time_map = {}
+    with open(logfile, 'r') as f:
+        for line in f:
+            if pattern.match(line):
+                separated = line.split()
+                tx_hash = separated[3]
+                confirm_time = int(separated[-1])
+                # extract the hash and time 
+                confirm_time_map[tx_hash] = confirm_time
 
-pattern = re.compile("^Confirmed trans hash")
-confirm_time_map = {}
+    for tx_hash in recv_time_map:
+        if tx_hash in confirm_time_map:
+            delay = confirm_time_map[tx_hash] - recv_time_map[tx_hash]
+            delay_map[tx_hash].add_entry(delay)
 
-with open(logfile, 'r') as f:
-    # lines = f.readlines()
-    for line in f:
-        if pattern.match(line):
-            separated = line.split()
-            tx_hash = separated[3]
-            confirm_time = int(separated[-1])
-            # extract the hash and time 
-            confirm_time_map[tx_hash] = confirm_time
-
-for tx_hash in recv_time_map:
-    if tx_hash in confirm_time_map:
-        delay =  confirm_time_map[tx_hash] - recv_time_map[tx_hash]
-        print(tx_hash, delay)
+for tx_hash in delay_map:
+    print(tx_hash, end = ' ')
+    delay_map[tx_hash].print_entry()
+            
